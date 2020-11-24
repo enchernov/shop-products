bee1abe5b2de30b4b0ecb0bec6bb4c717e364db1
enchernov87 <evgeniy.chernov2020@gmail.com>
2020-11-24 17:38:33 +0700

    add env

--- server/config/database.js
+++ server/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.w2j2h.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'shop-products'),
-        username: env('DATABASE_USERNAME', 'evgeniy'),
-        password: env('DATABASE_PASSWORD', 'developer2020'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
